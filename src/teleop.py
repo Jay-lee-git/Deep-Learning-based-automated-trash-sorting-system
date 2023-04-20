133b4345c4da4bab241bab262dcbc654030efa1e
jeheelee <jeheelee1@gmail.com>
2023-04-20 18:45:00 +0900

    refactoring

--- src/teleop.py
+++ src/teleop.py
@@ -1,135 +1,87 @@
-from dynamixel_sdk import * 
-import cv2
-import numpy as np
+from dynamixel_sdk import *
 import sys, tty, termios
-from datetime import datetime 
-import pyrealsense2 as rs
-def pos_to_angle(pos):
-    return int(pos/4095*365)
 
+
+class DynamixelControl:
+    def __init__(self, DXL_ID_list):
+        self.DEVICENUM = 5
+        self.DXL_MOVING_STATUS_THRESHOLD = 60
+        self.DEVICENAME = '/dev/ttyUSB0'
+        self.DXL_ID_list = DXL_ID_list
+        self.protocol_version = 2.0
+
+        self.portHandler_list = [PortHandler(self.DEVICENAME) for _ in range(self.DEVICENUM)]
+        self.packetHandler_list = [PacketHandler(self.protocol_version) for _ in range(self.DEVICENUM)]
+
+    
+    def open_port_and_baud(self):
+        self.TORQUE_ENABLE = 1
+        self.TORQUE_DISABLE = 0
+        self.ADDR_TORQUE_ENABLE = 64
+        self.baudrate = 1000000
+        for i in range(5):
+            if self.portHandler_list[i].openPort():
+                print("Succeeded to open the port | ", end="")
+            else:
+                print("Failed to open the port")
+                
+            if self.portHandler_list[i].setBaudRate(self.baudrate):
+                print("Succeeded to change the baudrate | ", end="")
+            else:
+                print("Failed to change the baudrate")
+
+            dxl_comm_result, dxl_error = self.packetHandler_list[i].write1ByteTxRx(self.portHandler_list[i], self.DXL_ID_list[i], self.ADDR_TORQUE_ENABLE, self.TORQUE_ENABLE)
+            if dxl_comm_result != COMM_SUCCESS:
+                print("%s" % self.packetHandler_list[i].getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("%s" % self.packetHandler_list[i].getRxPacketError(dxl_error))
+            else:
+                print("Dynamixel has been successfully connected")
+
+    def move_to_goal(self, dxl_goal_position):
+        ADDR_GOAL_POSITION = 116
+        for i in range(0, self.DEVICENUM):
+            dxl_comm_result, dxl_error = self.packetHandler_list[i].write4ByteTxRx(self.portHandler_list[i], 
+                                                                    self.DXL_ID_list[i], ADDR_GOAL_POSITION, dxl_goal_position[i])
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler_list[0].getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % self.packetHandler_list[0].getRxPacketError(dxl_error))
+
+    
 def angle_to_pos(angle):
     return int(4095/365 * angle)
 
-# Open port baudrate
-def open_port_and_baud(portHandler_list, index):
-    # Open port
-    if portHandler_list.openPort():        print(f"{index}-Succeeded to open the port | ", end=" ")
-    else:
-        print("Failed to open the port")
-        print("Press any key to terminate...")
-        getch()
-        quit()
-    # Set port baudrate
-    if portHandler_list.setBaudRate(BAUDRATE):
-        print("Succeeded to change the baudrate | ", end=" ")
-    else:
-        print("Failed to change the baudrate")
-        print("Press any key to terminate...")
-        getch()
-        quit()
-def realsense_config():
-    pipeline = rs.pipeline()
-    config = rs.config()
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-    pipeline.start(config)
-    return pipeline
-def get_realsense_frame(pipeline):
-    frames = pipeline.wait_for_frames()
-    color_frame = frames.get_color_frame()
-    color_image = np.asanyarray(color_frame.get_data())
-    return color_image
-# Enable Dynamixel Torque
-def enable_torque(packetHandler, portHandler_list, DXL_ID):
-    global ADDR_TORQUE_ENABLE, TORQUE_ENABLE, COMM_SUCCESS
-    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler_list, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-    elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
-    else:
-        print("Dynamixel has been successfully connected")
+    
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+    
 
-
-def get_camera_goal_pos(x1,y1, x2,y2,frame):
-    frame_height, frame_width, _ = frame.shape
-    # print('frane width and height', frame_width, frame_height)
-    frame_mid_point = (frame_width//2, frame_height//2)
-    # print(frame_mid_point)
-    target_mid_point = ((x1+x2)//2 -26, (y1+y2)//2 -2)
-    return frame_mid_point, target_mid_point
-
-if __name__ == '__main__':
-    ADDR_TORQUE_ENABLE          = 64
-    ADDR_GOAL_POSITION          = 116
-    ADDR_PRESENT_POSITION       = 132
-    DXL_MINIMUM_POSITION_VALUE  = 0         # Refer to the Minimum Position Limit of product
-    DXL_MAXIMUM_POSITION_VALUE  = 4095      # Refer to the Maximum Position Limit of product
-    BAUDRATE                    = 1000000
-    PROTOCOL_VERSION            = 2.0
-
-    TARGET_INDEX_CODE = 14.03
-    DXL_ID_list = [11, 12, 13, 14, 15]
-    DEVICE_NUM = len(DXL_ID_list)
-    DEVICENAME = '/dev/ttyUSB0'
-    TORQUE_ENABLE               = 1     # Value for enabling the torque
-    TORQUE_DISABLE              = 0     # Value for disabling the torque
-    DXL_MOVING_STATUS_THRESHOLD = 60    # Dynamixel moving status threshold
-    pipeline = realsense_config()
-    index = 0
-    target_pixel_threshold = 10
-    # Goal position
-    defalut_position = [angle_to_pos(180), angle_to_pos(100), angle_to_pos(250), angle_to_pos(180), angle_to_pos(180)]
-    dxl_goal_position = defalut_position
-    portHandler_list = [PortHandler(DEVICENAME) for _ in range(DEVICE_NUM)]
-    packetHandler_list = [PacketHandler(PROTOCOL_VERSION) for _ in range(DEVICE_NUM)]
-
-    for i in range(0, DEVICE_NUM):
-        open_port_and_baud(portHandler_list[i], i)
-        enable_torque(packetHandler_list[i], portHandler_list[i], DXL_ID_list[i])
-
-
-
+def main():
+    dxl_goal_position = [angle_to_pos(180), angle_to_pos(100), angle_to_pos(250), angle_to_pos(180), angle_to_pos(180)]
+    dynamixel = DynamixelControl([11, 12, 13, 14, 15])
+    dynamixel.open_port_and_baud()
     while True:
-        frame = get_realsense_frame(pipeline)
-        # Write goal position
-        for i in range(0, DEVICE_NUM):
-            dxl_comm_result, dxl_error = packetHandler_list[i].write4ByteTxRx(portHandler_list[i], 
-                                                                    DXL_ID_list[i], ADDR_GOAL_POSITION, dxl_goal_position[i])
-        if dxl_comm_result != COMM_SUCCESS:
-            print("%s" % packetHandler_list[0].getTxRxResult(dxl_comm_result))
-        elif dxl_error != 0:
-            print("%s" % packetHandler_list[0].getRxPacketError(dxl_error))
-        
-
-        key_input = cv2.waitKey(1)
+        key_input = ord(getch())
         if key_input == 27:
             break
         elif key_input == ord('w'):
-            dxl_goal_position[3] -= angle_to_pos(1)
+            dxl_goal_position[3] -= angle_to_pos(5)
         elif key_input == ord('s'):
-            dxl_goal_position[3] += angle_to_pos(1)
+            dxl_goal_position[3] += angle_to_pos(5)
         elif key_input == ord('a'):
-            dxl_goal_position[0] += angle_to_pos(1)
+            dxl_goal_position[0] += angle_to_pos(5)
         elif key_input == ord('d'):
-            dxl_goal_position[0] -= angle_to_pos(1)
-        elif key_input == ord('l'):
-            laser_flag = True
-        # out.write(frame)
-        cv2.imshow('frame', frame)
+            dxl_goal_position[0] -= angle_to_pos(5)
+        
+        dynamixel.move_to_goal(dxl_goal_position)
 
-        for i in range(0, DEVICE_NUM):
-            dxl_present_position, dxl_comm_result, dxl_error = packetHandler_list[i].read4ByteTxRx(portHandler_list[i], DXL_ID_list[i], ADDR_PRESENT_POSITION)
-            if dxl_comm_result != COMM_SUCCESS:     
-                print("%s" % packetHandler_list[i].getTxRxResult(dxl_comm_result))
-            elif dxl_error != 0:
-                print("%s" % packetHandler_list[i].getRxPacketError(dxl_error))
-           
-    # Disable Dynamixel Torque
-    for i in range(0, DEVICE_NUM):
-        dxl_comm_result, dxl_error = packetHandler_list[i].write1ByteTxRx(portHandler_list[i], DXL_ID_list [i], ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
-
-    # Close port
-    portHandler_list[0].closePort()
-    # cap.release()
-    # out.release()
+if __name__ == '__main__':
+    main()
