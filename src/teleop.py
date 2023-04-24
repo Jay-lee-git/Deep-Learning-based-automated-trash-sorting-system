d75a0429472c46ed84f3ac98618d9867fd79d964
jeheelee <jeheelee1@gmail.com>
2023-04-24 14:56:36 +0900

    create ik

--- src/teleop.py
+++ src/teleop.py
@@ -1,5 +1,49 @@
 from dynamixel_sdk import *
 import sys, tty, termios
+from ikpy.chain import Chain
+from ikpy.link import OriginLink, URDFLink
+from collections import deque
+
+open_maipulator = Chain(name='test_arm', links=[
+    OriginLink(),
+    URDFLink(
+      
+      name="link0",
+      # origin_translation=[0.012, 0, 0.017],
+      origin_translation=[0, 0, 0.003],
+      origin_orientation=[0, 0, 0],
+      rotation=[0, 1, 0],
+      use_symbolic_matrix = False,
+      joint_type = "revolute"
+    ),    
+    URDFLink(
+      name="link1",
+      # origin_translation=[0.012, 0, 0.017],
+      origin_translation=[0, 0, 0.013],
+      origin_orientation=[0, 0, 0],
+      rotation=[0, 1, 0],
+      use_symbolic_matrix = False,
+      joint_type = "revolute"
+    ),
+    URDFLink(
+      name="link2",
+      # origin_translation=[0.012, 0, 0.017],
+      origin_translation=[0.013, 0, 0],
+      origin_orientation=[0, 0, 0],
+      rotation=[0, 1, 0],
+      use_symbolic_matrix = False,
+      joint_type = "revolute"
+    ),    
+    URDFLink(
+      name="link3",
+      # origin_translation=[0, 0, 0.0595],
+      origin_translation=[0.013, 0, 0],
+      origin_orientation=[0, 0, 0],
+      rotation=[0, 1, 0],
+      use_symbolic_matrix = False,
+      joint_type = "revolute"
+    ),
+])
 
 
 class DynamixelControl:
@@ -52,10 +96,16 @@ class DynamixelControl:
         for i in range(0, self.DEVICENUM):
             dxl_comm_result, dxl_error = self.packetHandler_list[i].write1ByteTxRx(self.portHandler_list[i], self.DXL_ID_list [i], self.ADDR_TORQUE_ENABLE, self.TORQUE_DISABLE)
             self.portHandler_list[i].closePort()
-    
+    # def solve_ik(self, dxl_comm_result):
+    #     print(dxl_comm_result)
+
+
+
 def angle_to_pos(angle):
     return int(4095/365 * angle)
 
+def pos_to_angle(pos):
+    return int(pos*365 /4095)
     
 def getch():
     fd = sys.stdin.fileno()
@@ -68,11 +118,35 @@ def getch():
     return ch
     
 
+import numpy as np
+
+
+# def list_to_angle(arr):
+    # return [pos_to_angle(i) for i in arr]
+
+def list_to_angle(arr):
+    return [angle_to_pos((pos_to_angle(i)*180/np.pi)+180) for i in arr]
+
+
 def main():
-    dxl_goal_position = [angle_to_pos(180), angle_to_pos(100), angle_to_pos(250), angle_to_pos(180), angle_to_pos(180)]
+    # with open("output.csv", "w", newline="") as csvfile:
+    mean_pos = [deque(), deque(), deque(), deque(), deque()]
+    target_vector = [0.025605  , 0., 0.02051485]
+    dxl_goal_position = [angle_to_pos(180), angle_to_pos(170), angle_to_pos(180), angle_to_pos(180), angle_to_pos(50)]
     dynamixel = DynamixelControl([11, 12, 13, 14, 15])
     dynamixel.open_port_and_baud()
     while True:
+
+        tmp_goal = [angle_to_pos(i+180) for i in open_maipulator.inverse_kinematics(target_vector)*180/np.pi]
+        print([abs(dxl_goal_position[i]- tmp_goal[i]) for i in range(5)])
+        for i in range(5):
+            mean_pos[i].append(tmp_goal[i])
+
+            if len(mean_pos[i]) > 5:
+                mean_pos[i].popleft()
+
+            dxl_goal_position[i] = sum(mean_pos[i])//len(mean_pos[i])
+
         key_input = ord(getch())
         if key_input == ord('q'):
             break
@@ -84,9 +158,22 @@ def main():
             dxl_goal_position[0] += angle_to_pos(5)
         elif key_input == ord('d'):
             dxl_goal_position[0] -= angle_to_pos(5)
+        elif key_input == ord('j'):
+            target_vector[0] += 0.0005
+        elif key_input == ord('l'):
+            target_vector[0] -= 0.0005
+
+        elif key_input == ord('i'):
+            target_vector[2] += 0.0005
+        elif key_input == ord('k'):
+            target_vector[2] -= 0.0005
         
+        # if sum(safety_flag):   
         dynamixel.move_to_goal(dxl_goal_position)
-        
+        # print(dxl_goal_position)
+
+     
+
     dynamixel.kill_process()
 
 
